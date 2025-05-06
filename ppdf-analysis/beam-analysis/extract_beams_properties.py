dc98cabd7617c9ca865431fb1e7878f18887cf3f
Fang Han <fhanonline@gmail.com>
2025-05-06 18:00:29 -0400

    update beam extraction

--- ppdf-analysis/beam-analysis/extract_beams_properties.py
+++ ppdf-analysis/beam-analysis/extract_beams_properties.py
@@ -1,14 +1,29 @@
 import os
 
+from torch import cat, tensor
+
+from beam_properties import (
+    angular_edges_on_arc,
+    # beams_properties_2d,
+    beams_basic_properties,
+    beams_line_properties,
+    sample_ppdf_on_arc_2d_local,
+)
 from convex_hull_helper import convex_hull_2d, sort_points_for_hull_batch_2d
 from geometry_2d_io import load_scanner_layout_geometries, load_scanner_layouts
-from geometry_2d_utils import fov_tensor_dict
+from geometry_2d_utils import (
+    fov_tensor_dict,
+    pixels_coordinates,
+    pixels_to_detector_unit_rads,
+)
 from ppdf_io import load_ppdfs_data_from_hdf5
 
 if __name__ == "__main__":
 
     scanner_layouts_dir = "../../../pymatcal/scanner_layouts"
-    scanner_layouts_filename = "scanner_layouts_77faff53af5863ca146878c7c496c75e.tensor"
+    scanner_layouts_filename = (
+        "scanner_layouts_77faff53af5863ca146878c7c496c75e.tensor"
+    )
 
     ppdfs_dataset_dir = (
         "../../../../data/scanner_layouts_77faff53af5863ca146878c7c496c75e"
@@ -19,26 +34,45 @@ if __name__ == "__main__":
         scanner_layouts_dir, scanner_layouts_filename
     )
     # Load the PPDFs data
-    fov_tensor_dict = fov_tensor_dict(
+    fov_dict = fov_tensor_dict(
         n_pixels=(512, 512),
         mm_per_pixel=(0.25, 0.25),
         center_coordinates=(0.0, 0.0),
     )
+    fov_points_2d = pixels_coordinates(fov_dict)
 
     # Loop through the scanner positions (0 to 23)
     for layout_idx in range(1):
         # Load the scanner geometry
-        plates_vertices, detector_units_vertices = load_scanner_layout_geometries(
-            layout_idx, scanner_layouts_data
+        plates_vertices, detector_units_vertices = (
+            load_scanner_layout_geometries(layout_idx, scanner_layouts_data)
         )
 
         # Set the PPDFs filename for a particular scanner position
-        ppdfs_hdf5_filename = "position_000_ppdfs.hdf5"
+        ppdfs_hdf5_filename = f"position_{layout_idx:03d}_ppdfs.hdf5"
 
         # Load the PPDFs data
         ppdfs = load_ppdfs_data_from_hdf5(
-            ppdfs_dataset_dir, ppdfs_hdf5_filename, fov_tensor_dict
+            ppdfs_dataset_dir, ppdfs_hdf5_filename, fov_dict
         )
+
+        detector_unit_centers = detector_units_vertices.mean(dim=1)
+        fov_corners = (
+            tensor([[-1, -1], [1, -1], [1, 1], [-1, 1]])
+            * fov_dict["size in mm"]
+            * 0.5
+        )
+
+        hull_points_batch = cat(
+            (
+                fov_corners.unsqueeze(0).expand(
+                    detector_units_vertices.shape[0], -1, -1
+                ),
+                detector_unit_centers.unsqueeze(1),
+            ),
+            dim=1,
+        )
+        hull_points_batch = sort_points_for_hull_batch_2d(hull_points_batch)
 
         # Print the shapes of the loaded data
         print(
@@ -47,4 +81,42 @@ if __name__ == "__main__":
             + f"Detector units shape: {list(detector_units_vertices.shape)}\n"
             + f"PPDFs data shape:     {list(ppdfs.shape)}"
         )
-        
+        n_detector_units = detector_units_vertices.shape[0]
+
+        # Loop through the detector units
+        for detector_unit_idx in range(n_detector_units):
+
+            # Get the convex hull for the current detector unit
+            hull_2d = convex_hull_2d(hull_points_batch[detector_unit_idx])
+
+            # Get the PPDF data for the current detector unit
+            ppdf_data_2d = ppdfs[detector_unit_idx].view(
+                int(fov_dict["n pixels"][0]), int(fov_dict["n pixels"][1])
+            )
+            (sampled_ppdf, sampling_rads, sampling_points) = (
+                sample_ppdf_on_arc_2d_local(
+                    ppdf_data_2d,
+                    detector_unit_centers[detector_unit_idx],
+                    hull_2d,
+                    fov_dict,
+                )
+            )
+
+            relative_sampled_ppdf = sampled_ppdf / sampled_ppdf.max()
+            radian_edges = angular_edges_on_arc(
+                sampled_ppdf,
+                sampling_rads,
+            )
+            fov_points_rads = pixels_to_detector_unit_rads(
+                fov_points_2d,
+                detector_unit_centers[detector_unit_idx],
+            )
+            beams_properties = beams_basic_properties(
+                ppdfs[detector_unit_idx],
+                fov_points_2d,
+                fov_points_rads,
+                radian_edges,
+                detector_unit_centers[detector_unit_idx],
+            )
+            print(f"Detector unit {detector_unit_idx}:\n"
+                  +f"Number of beams: {beams_properties['number of beams']}\n")
--- ppdf-analysis/beam-analysis/geometry_2d_utils.py
+++ ppdf-analysis/beam-analysis/geometry_2d_utils.py
@@ -1,6 +1,9 @@
+from typing import Dict, Sequence
+
+from torch import Tensor, arange, atan2, float32
+from torch import max as torch_max
+from torch import meshgrid, pi, stack, tensor
 from torch.nn import functional as F
-from torch import max as torch_max, tensor, Tensor
-from typing import Sequence
 
 
 def local_max_1d(data: Tensor, size: int = 3):
@@ -28,3 +31,35 @@ def fov_tensor_dict(
     }
     fov_dict["size in mm"] = fov_dict["n pixels"] * fov_dict["mm per pixel"]
     return fov_dict
+
+
+def pixels_coordinates(
+    fov_dict: Dict,
+) -> Tensor:
+
+    pixel_indices = stack(
+        meshgrid(
+            arange(0, int(fov_dict["n pixels"][0])),
+            arange(0, int(fov_dict["n pixels"][1])),
+            indexing="ij",
+        ),
+        dim=2,
+    ).view(-1, 2)
+    return (
+        pixel_indices.to(dtype=float32) * fov_dict["mm per pixel"]
+        - fov_dict["size in mm"] * 0.5
+        + fov_dict["center coordinates in mm"]
+    )
+
+
+def pixels_to_detector_unit_rads(
+    pixel_coordinates: Tensor,
+    detector_unit_center: Tensor,
+) -> Tensor:
+
+    pixel_rads = atan2(
+        pixel_coordinates[:, 1] - detector_unit_center[1],
+        pixel_coordinates[:, 0] - detector_unit_center[0],
+    )
+    pixel_rads = pixel_rads + 2 * pi * (pixel_rads < 0)
+    return pixel_rads
